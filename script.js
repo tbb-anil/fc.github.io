2049da2e916604a90815ebc20bf682e817abda27
AnilChapagain <chapagain_anil@binarybizz.com>
2018-11-15 21:34:37 +0530

    function constucter

---
+++ script.js
@@ -0,0 +1,13 @@
+var john = {
+    name:'John',
+    yearOfBirth: 1990,
+    job: 'teacher',
+
+};
+var Person = function (name,yearOfBirth,job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+
+};
+var john = new Person('John', 1990, 'teacher');
