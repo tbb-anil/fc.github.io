ab50ffb64bef3c0e0b58d35c0e2e76540db991bb
AnilChapagain <chapagain_anil@binarybizz.com>
2018-11-15 22:11:58 +0530

    index

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ var Person = function (name,yearOfBirth,job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calculateAge = function () {
-        console.log(2016 - this.yearOfBirth);
-    }
+};
+Person.prototype.calculateAge = function () {
+    console.log(2018 - this.yearOfBirth);
 
 };
+Person.prototype.lastName = 'Smith';
 var john = new Person('John', 1990, 'teacher');
-john.calculateAge();
 var jane = new Person('Jane', 1969, 'Designer');
 var mark = new Person('Mark', 1948, 'Retired');
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
